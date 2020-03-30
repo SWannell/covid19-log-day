fc69710174c89c143d4f7193d1f15a7ca8d91e21
Sami Capulcu <setatimmi@gmail.com>
2020-03-30 11:43:33 +0100

    Added annotated line graphs Added diff-in-diff graphs for UK Added exploratory UK rate of change / new cases graphs Allowed country list force on CSV munge Hid empty small mult panels Formatted % axis

--- by_day_JH.py
+++ by_day_JH.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Mar 26 01:10:00 2020
-
 @author: setat
 """
 
 import pandas as pd
 import matplotlib.pyplot as plt; plt.style.use('ggplot')
+import matplotlib.ticker as mtick
 import math
 import numpy as np
 
@@ -20,11 +20,14 @@ day_start_cases = 100
 # metric, cases_over = 'deaths', 100
 
 
-def munge_csv(fp, metric, case_floor):
+def munge_csv(fp, metric, case_floor, countries_specified=None):
     df = pd.read_csv(fp.format(metric))
     df = df.groupby('Country/Region').sum()
     df.drop(['Lat', 'Long'], axis=1, inplace=True)
-    df = df[df.iloc[:, -1] > case_floor]
+    if countries_specified is not None:
+        df = df.filter(country_list, axis=0)
+    else:
+        df = df[df.iloc[:, -1] > case_floor]
     df = df.T
     df.index = pd.to_datetime(df.index)
     df.to_csv('AmendedData\\{}ByCountry.csv'.format(metric.title()))
@@ -72,10 +75,35 @@ def plot_mults(country_list, data, el_count, x, y, metric):
         ax.plot(data[country], color=c, linewidth=3)
         ax.set_title(country)
         ax.set_yscale('log')
+    empty_count = x*y - el_count
+    if empty_count > 0:
+        for ax in axs.flatten()[-empty_count:]:
+            ax.set_visible(False)
     plt.suptitle('{} for top {} countries'.format(metric.title(), el_count),
                  fontsize=30)
-    plt.xlabel('Days since 100 mark passed')
+    plt.xlabel('Days since {} mark passed'.format(day_start_cases))
     plt.savefig('Outputs\\{}.png'.format(out_fp))
+    plt.show()
+
+
+def plot_annotated(data, el_count, metric):
+    fig, ax = plt.subplots(1, 1, figsize=(10,10))
+    data.plot(ax=ax, colormap='Dark2')
+    labels = {}
+    for country in data.columns:
+        _ = data[country].dropna()
+        valid_days_from_zero = len(_)-1
+        labels[country] = (valid_days_from_zero, _.loc[valid_days_from_zero])
+    top_counts = from_case_start.sum().sort_values(ascending=False)
+    uk_position = list(top_counts.index).index('United Kingdom')
+    only_label = top_counts.index[:uk_position+1]
+    for country in only_label:
+        ax.annotate(country, labels[country])
+    plt.title('{} for top {} countries'.format(metric.title(), el_count),
+              fontsize=30)
+    plt.xlabel('Days since {} mark passed'.format(day_start_cases))
+    plt.savefig('Outputs\\{}_annotated.png'.format(out_fp))
+    plt.show()
 
 
 # Get country list from deaths, plot deaths
@@ -90,6 +118,7 @@ dct = count_from_day_x(df, country_list)
 from_case_start = counts_to_df(dct, country_list, out_fp)
 (x, y) = small_mult_size(country_count)
 plot_mults(country_list, from_case_start, country_count, x, y, metric)
+plot_annotated(from_case_start, country_count, metric)
 
 # Plot just UK
 country = "United Kingdom"
@@ -107,15 +136,39 @@ plt.legend()
 plt.savefig('Outputs\\{}In{}From{}thCaseDay.png'.format(metric.title(),
             country.replace(' ', ''), day_start_cases))
 
+# Plot diff-in-diff
+figs, axs = plt.subplots(3, 1, figsize=(8,12))
+_ = df['United Kingdom'][df['United Kingdom'] > 100]
+ttls = ['{} per day', 'New {} per day', '{} diff-in-new per day']
+for ax, ttl in zip(axs.flatten(), ttls):
+    _.plot(ax=ax)
+    ax.set_title(ttl.format(metric))
+    _ = _.diff()
+plt.suptitle('UK daily changes', fontsize=30, y=0.95)
+plt.savefig('Outputs\\UKDailyChanges.png')
+
 # Use previous country list, plot confirmed
 metric = 'confirmed'
 out_fp = '{}ByCountryFrom{}thCaseDay'.format(metric.title(),
                                              day_start_cases)
-df = munge_csv(fp, metric, cases_over[metric])
+df = munge_csv(fp, metric, cases_over[metric],
+               countries_specified=country_list)
 dct = count_from_day_x(df, country_list)
 from_case_start = counts_to_df(dct, country_list, out_fp)
 (x, y) = small_mult_size(country_count)
 plot_mults(country_list, from_case_start, country_count, x, y, metric)
+plot_annotated(from_case_start, country_count, metric)
 
-# Should get whole df, then filter on deaths, then get country list. Then
-# store country list, and use it for both filters and plots
+# UK rate of change per day
+uk_rate_change = df['United Kingdom'] / df['United Kingdom'].shift(1)
+uk_rate_change = uk_rate_change - 1
+ax_ukrate = uk_rate_change.plot()
+ax_ukrate.set_title('UK {} rate change by day'.format(metric))
+ax_ukrate.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1))
+plt.show()
+
+# UK new cases by day
+uk_new_confirmed = df['United Kingdom'].diff()
+uk_new_confirmed.plot(style='.-', figsize=(10, 6))
+plt.title('UK new {} cases by day'.format(metric))
+plt.show()
